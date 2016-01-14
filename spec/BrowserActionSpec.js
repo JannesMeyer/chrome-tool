aed17662194c731d9c50e35cd2620583f9dfa4a3
Jannes <jannes.meyer@gmail.com>
2016-01-14 22:21:02 +0000

    Add first test

--- spec/BrowserActionSpec.js
+++ spec/BrowserActionSpec.js
@@ -3,12 +3,16 @@ global.chrome = {
     getTitle() {}
   }
 };
+
 var BrowserAction = require('../browser-action');
 
 describe('BrowserAction', () => {
 
-  it('calls through correctly', () => {
+  beforeEach(() => {
     spyOn(chrome.browserAction, 'getTitle');
+  });
+
+  it('calls through correctly', () => {
     BrowserAction.getTitle('foo');
     expect(chrome.browserAction.getTitle).toHaveBeenCalledWith('foo', jasmine.any(Function));
   });
