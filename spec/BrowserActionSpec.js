64e7be620a4ce07d37ab1b3de95b9f54bf2ef217
Jannes <jannes.meyer@gmail.com>
2016-01-14 22:27:02 +0000

    Add second test

--- spec/BrowserActionSpec.js
+++ spec/BrowserActionSpec.js
@@ -1,6 +1,7 @@
 global.chrome = {
   browserAction: {
-    getTitle() {}
+    getTitle() {},
+    setBadgeText() {},
   }
 };
 
@@ -10,11 +11,18 @@ describe('BrowserAction', () => {
 
   beforeEach(() => {
     spyOn(chrome.browserAction, 'getTitle');
+    spyOn(chrome.browserAction, 'setBadgeText');
   });
 
   it('calls through correctly', () => {
     BrowserAction.getTitle('foo');
     expect(chrome.browserAction.getTitle).toHaveBeenCalledWith('foo', jasmine.any(Function));
+  });
+
+  it('returns a promise', () => {
+    let result = BrowserAction.setBadgeText('test');
+
+    expect(chrome.browserAction.setBadgeText).toHaveBeenCalledWith('test', jasmine.any(Function));
   });
 
 });
