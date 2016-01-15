0afa119830f66fdca760c7912adc3a8b2e9f05dc
Jannes <jannes.meyer@gmail.com>
2016-01-15 00:10:32 +0000

    Refactor dechromeify() to not use the non-standard Promise.defer()

--- src/dechromeify.js
+++ src/dechromeify.js
@@ -1,29 +1,20 @@
 /**
- * Convert an async Chrome function into one that returns a Promise
+ * Convert an async callback-style function into one that returns a Promise
  */
 export function dechromeify(source, name) {
-  return function dechromeified() {
-    var deferred = Promise.defer();
-
-    // Add callback as last argument
-    Array.prototype.push.call(arguments, function callback() {
-      if (chrome.runtime.lastError) {
-        deferred.reject(chrome.runtime.lastError);
-      } else {
-        deferred.resolve(arguments[0]);
-      }
+  return function promisified() {
+    let args = Array.prototype.slice.call(arguments);
+    return new Promise((resolve, reject) => {
+      // Add callback as last argument
+      args.push(response => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(response);
+        }
+      });
+      source[name].apply(source, args);
     });
-
-    // Execute function
-    source[name].apply(source, arguments);
-
-    return deferred.promise;
-  };
-}
-
-export function alias(source, name) {
-  return function aliased() {
-    return source[name].apply(source, arguments);
   };
 }
 
@@ -32,15 +23,33 @@ export function alias(source, name) {
  */
 export function dechromeifyAll(source, sync = []) {
   let result = Object.create(null);
+
   Object.keys(source).forEach(name => {
+    // Function
     if (typeof source[name] === 'function') {
       let convertFunction = (sync.indexOf(name) > -1 ? alias : dechromeify);
       result[name] = convertFunction(source, name);
-    } else if (source[name] instanceof chrome.Event) {
-      result[name] = alias(source[name], 'addListener');
-    } else {
-      result[name] = source[name];
+      return;
     }
+
+    // Event
+    if (source[name] instanceof chrome.Event) {
+      result[name] = alias(source[name], 'addListener');
+      return;
+    }
+
+    // Anything else
+    result[name] = source[name];
   });
+
   return result;
+}
+
+/**
+ * Rename a sync function (closures are faster than .bind)
+ */
+export function alias(source, name) {
+  return function aliased() {
+    return source[name].apply(source, arguments);
+  };
 }
