acb46145e76020a84ce737d69506201fbcc9829d
Jannes <jannes.meyer@gmail.com>
2016-01-17 00:41:34 +0000

    Remove timing test

--- spec/ChromeToolSpec.js
+++ spec/ChromeToolSpec.js
@@ -1,20 +1,11 @@
 'use strict';
 
-// Note: This has to be the first spec file being executed
 describe('chrome-tool', () => {
 
   var chrometool;
-  var loadTime;
 
   beforeAll(() => {
-    let startTime = new Date().getTime();
     chrometool = require('..');
-    loadTime = new Date().getTime() - startTime;
-  });
-
-  it('loads in less than 20ms', () => {
-    expect(loadTime).not.toBe(0);
-    expect(loadTime).toBeLessThan(20);
   });
 
 });
